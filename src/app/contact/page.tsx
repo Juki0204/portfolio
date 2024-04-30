b1ac087563add7e5fab679e39dedaf65c9cf6e72
Juki0204 <jukid0204@yahoo.co.jp>
2024-04-30 20:58:42 +0900

    お問い合わせフォーム実装

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
   return (
     <main className="max-w-[640px] w-full m-auto overflow-x-hidden">
-      <section className="mt-[60px] p-7 mb-5 relative z-10 text-center overflow-hidden">
+      <section className="min-h-lvh mt-[60px] p-7 mb-5 relative z-10 text-center overflow-hidden">
         <SecTitle en="Contact" jp="おといあわせ" className="mb-5" />
         <ContactForm />
 
